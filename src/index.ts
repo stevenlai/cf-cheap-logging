3ee041c2a1f6d90ec05ca9b9385713a9145ea608
Steven Lai <lai.steven@gmail.com>
2022-12-15 10:21:59 -0500

    example of wrapping cf worker in a logging function to offload elsewhere

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `wrangler dev src/index.ts` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export interface Env {
+	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+	// MY_KV_NAMESPACE: KVNamespace;
+	//
+	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	//
+	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+	// MY_BUCKET: R2Bucket;
+}
+
+export default {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<Response> {
+		return new Response(logFunc("Hello", "john"));
+	},
+};
+
+// Whatever my worker is doing
+const handleRequest = (s: string, c: string) => {
+	return s+c
+}
+
+// Logger function
+const cheapLogger = (a: Function): Function => {
+	return (...args: any[]) => {
+		console.log("Send logs somewhere.")
+		return a(...args)
+	}
+}
+
+// Wrap the worker function with the logger
+const logFunc = cheapLogger(handleRequest);
